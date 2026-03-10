ad864ae5ad6334666e4af79ef1a4448dfd665648
Rikorex <cohen.sinainai@gmail.com>
2026-03-10 17:11:03 +0200

    Added basic movement

---
+++ cat somthing box/Assets/playermovementbasic.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+
+public class playermovementbasic : MonoBehaviour
+{
+    private float movespeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveinput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveinput * movespeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveinput = context.ReadValue<Vector2>();
+    }
+}
+
